a1229ba3e56506854f070a180f18132f245435d0
Gakumi Reo <42238843+reo-g@users.noreply.github.com>
2021-10-24 05:17:20 +0900

    suica以外の場合でも読み取れるように変更

--- nfc_test/nfc_read_test.py
+++ nfc_test/nfc_read_test.py
@@ -5,24 +5,25 @@ import nfc
 def main():
   clf = nfc.ContactlessFrontend('usb')
   # 212F(FeliCa)
-  target_req = nfc.clf.RemoteTarget("212F")
+  target_suica = nfc.clf.RemoteTarget("212F")
   # 0003(Suica)
-  target_req.sensf_req = bytearray.fromhex("0000030000")
+  target_suica.sensf_req = bytearray.fromhex("0000030000")
 
-  # 212F(FeliCa) 学生証用に2つ起動させておく
-  target_req2 = nfc.clf.RemoteTarget("212F")
+  # 212F(FeliCa) 学生証用に2つ起動させておく(suica以外のカード)
+  target_req = nfc.clf.RemoteTarget("212F")
 
+
+  target = None
   while True:
-    target_res = clf.sense(target_req, iterations=10, interval=0.01)
-    target_res2 = clf.sense(target_req2, iterations=10, interval=0.01)
+    target_res = clf.sense(target_suica, iterations=10, interval=0.01)
+    target_res2 = clf.sense(target_req, iterations=10, interval=0.01)
     if target_res is not None:
-        tag = nfc.tag.activate(clf,target_res)
-        tag.sys = 3
-        idm = binascii.hexlify(tag.idm)
-        print(idm)
+        target = target_res
         break
-    if target_res2 is not None:
-        tag = nfc.tag.activate(clf,target_res2)
+    elif target_res2 is not None:
+        target = target_res2
+    if target is not None:
+        tag = nfc.tag.activate(clf,target)
         tag.sys = 3
         idm = binascii.hexlify(tag.idm)
         print(idm)
