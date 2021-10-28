22c727e0c47827685ef19afd7f4826016afc3ef8
Gakumi Reo <42238843+reo-g@users.noreply.github.com>
2021-10-28 13:51:46 +0900

    change servo pin for test file

--- test_code/servo_test.py
+++ test_code/servo_test.py
@@ -6,8 +6,12 @@ import time
 
 pi = pigpio.pi()
 
-pi.set_servo_pulsewidth(4, 700)
+pi.set_servo_pulsewidth(18, 700)
+time.sleep(0.1)
+pi.set_servo_pulsewidth(18, 0)
 time.sleep(3)
 
-pi.set_servo_pulsewidth(4, 2300)
+pi.set_servo_pulsewidth(18, 2300)
+time.sleep(0.1)
+pi.set_servo_pulsewidth(18, 0)
 time.sleep(3)
