a85807069cd4e20428f5d08df68568b3a557d28c
Saza-ku <the.latticeheart@gmail.com>
2023-05-24 21:37:37 +0900

    implement test hook

--- hook.py
+++ hook.py
@@ -7,17 +7,7 @@ def open_door_hook():
     show_datetime()
 
 def show_datetime(): #「日時」を表示
-    # Main program block
-    # Initialise display
-    lcd_init()
-    # while True:
-    for _ in range(3600):
-      local_time = datetime.datetime.now()
-      lcd_string(strftime("%Y.%m.%d (%a)", gmtime()) , LCD_LINE_1)
-      lcd_string(local_time.strftime("%H:%M"), LCD_LINE_2)
-      sleep(1)
-
-      local_time = datetime.datetime.now()
-      lcd_string(strftime("%Y.%m.%d (%a)", gmtime()) , LCD_LINE_1)
-      lcd_string(local_time.strftime("%H %M"), LCD_LINE_2)
-      sleep(1)
+    local_time = datetime.datetime.now()
+    lcd_string(strftime("%Y.%m.%d (%a)", gmtime()) , LCD_LINE_1)
+    lcd_string(local_time.strftime("%H:%M"), LCD_LINE_2)
+    sleep(1)
