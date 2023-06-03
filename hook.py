71524f783c95e63743c7fcacb6b048a3b4a58441
arrtic-plus <artichoke.exe@gmail.com>
2023-06-03 15:21:36 +0900

    separate specific operations to remp_api.py

--- hook.py
+++ hook.py
@@ -1,48 +1,21 @@
 import datetime
 from time import sleep
 from time import gmtime, strftime
-import http.client
-import os
 
 from lib.lcd import lcd_string
 import lib.lcd as lcd
 
+from remo_api import aircon_on, aircon_off
+
 def close_door_hook():
     show_datetime()
 
-    # 以下 REMO経由でACを落とすコード
-    conn = http.client.HTTPSConnection("api.nature.global")
-
-    payload = "button=power-off"
-
-    headers = {
-      'accept': "application/json",
-      'Content-Type': "application/x-www-form-urlencoded",
-      'Authorization': "Bearer " + os.environ["REMO_TOKEN"]
-    }
-
-    conn.request("POST", "/1/appliances/19c992ee-1bf3-4270-89a3-5f86ab7b9ad7/aircon_settings", payload, headers)
-
-    conn.getresponse()
+    aircon_off()
 
 def open_door_hook():
     show_datetime()
 
-    # 以下 REMO経由でACをつけるコード
-    conn = http.client.HTTPSConnection("api.nature.global")
-
-    payload = "button=power-on"
-
-    headers = {
-      'accept': "application/json",
-      'Content-Type': "application/x-www-form-urlencoded",
-      'Authorization': "Bearer " + os.environ["REMO_TOKEN"]
-    }
-
-    conn.request("POST", "/1/appliances/19c992ee-1bf3-4270-89a3-5f86ab7b9ad7/aircon_settings", payload, headers)
-
-    conn.getresponse()
-
+    aircon_on()
 
 
 def show_datetime(): #「日時」を表示
